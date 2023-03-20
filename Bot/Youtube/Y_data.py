da274c2a307810c3e059a4e273ab966441bf7489
Admin <abhihkakadiya2301@gmail.com>
2023-03-20 15:24:35 +0530

    Some title printed

--- Bot/Youtube/Y_data.py
+++ Bot/Youtube/Y_data.py
@@ -3,23 +3,25 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
-driver.get("https://www.youtube.com/")
+driver.get("https://www.youtube.com/@statquest/videos")
 driver.maximize_window()
 driver.implicitly_wait(10)
 
-search_element = driver.find_element(By.CSS_SELECTOR,"input")
+'''search_element = driver.find_element(By.CSS_SELECTOR,"input")
 search_element.click()
 search_element.send_keys("statquest")
 search_element.send_keys(Keys.ENTER)
 
-enter_channel = driver.find_element(By.ID,'main-link').click()
+enter_channel = driver.find_element(By.ID,'main-link').click()'''
 video_tab = driver.find_element(By.XPATH,"//*[@id=\"tabsContent\"]/tp-yt-paper-tab[2]").click()
 
-list = []
 video_title = driver.find_elements(By.CLASS_NAME,"style-scope ytd-rich-grid-renderer")
 for i in video_title:
-    title = i.find_element(By.ID,"video-title").text
-    print(title)
+    row = i.find_elements(By.CLASS_NAME,"style-scope ytd-rich-grid-row")
+    for j in row:
+        title = j.find_element(By.ID,"video-title").text
+        print(title)
+
 
 
 
