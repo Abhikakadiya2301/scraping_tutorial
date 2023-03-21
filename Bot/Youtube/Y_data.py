252786f2d356da73c0e5a5614e07e0ec4c6d09ff
Admin <abhihkakadiya2301@gmail.com>
2023-03-21 10:09:37 +0530

    Complete code without Error

--- Bot/Youtube/Y_data.py
+++ Bot/Youtube/Y_data.py
@@ -1,26 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
 
 driver = webdriver.Chrome()
-driver.get("https://www.youtube.com/@statquest/videos")
+driver.get("https://www.youtube.com/")
 driver.maximize_window()
 driver.implicitly_wait(10)
 
-'''search_element = driver.find_element(By.CSS_SELECTOR,"input")
+search_element = driver.find_element(By.CSS_SELECTOR,"input")
 search_element.click()
 search_element.send_keys("statquest")
 search_element.send_keys(Keys.ENTER)
 
-enter_channel = driver.find_element(By.ID,'main-link').click()'''
+enter_channel = driver.find_element(By.ID,'main-link').click()
 video_tab = driver.find_element(By.XPATH,"//*[@id=\"tabsContent\"]/tp-yt-paper-tab[2]").click()
 
 video_title = driver.find_elements(By.CLASS_NAME,"style-scope ytd-rich-grid-renderer")
+time.sleep(10)
 for i in video_title:
-    row = i.find_elements(By.CLASS_NAME,"style-scope ytd-rich-grid-row")
+    row = i.find_elements(By.CLASS_NAME,"style-scope ytd-rich-grid-media")
     for j in row:
         title = j.find_element(By.ID,"video-title").text
         print(title)
+driver.quit()
 
 
 
